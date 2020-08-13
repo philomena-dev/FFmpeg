deb0e6e93691cbd5a1e8cbb08ee356d6f64be2d8
byte[] <byteslice@airmail.cc>
2020-08-13 16:26:48 -0400

    disallow reading negative lengths

--- libavcodec/mjpegdec.c
+++ libavcodec/mjpegdec.c
@@ -1814,6 +1814,10 @@ static int mjpeg_decode_app(MJpegDecodeContext *s)
 
     len = get_bits(&s->gb, 16);
 
+    if (len < 0) {
+        return AVERROR_INVALIDDATA;
+    }
+
     if (len < 6) {
         skip_bits(&s->gb, len);
 
